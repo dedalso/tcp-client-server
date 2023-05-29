b66a9ea066f1e2c0d96f6f26d542904efc4520c3
So. Game Studios <sorogincompany@gmail.com>
2023-05-29 05:19:02 +0500

    Started with backend

--- DefaultForm.Designer.cs
+++ DefaultForm.Designer.cs
@@ -31,19 +31,20 @@
             labelDescription = new Label();
             labelMode = new Label();
             groupBoxSetup = new GroupBox();
-            buttonDisconnect = new Button();
-            buttonConnect = new Button();
-            textBoxPort = new TextBox();
-            labelPort = new Label();
-            textBox2 = new TextBox();
-            labelIP = new Label();
-            radioButtonModeServer = new RadioButton();
             radioButtonModeClient = new RadioButton();
+            radioButtonModeServer = new RadioButton();
+            labelIP = new Label();
+            textBoxIP = new TextBox();
+            labelPort = new Label();
+            textBoxPort = new TextBox();
+            buttonConnect = new Button();
+            buttonDisconnect = new Button();
             groupBoxSend = new GroupBox();
-            button1 = new Button();
+            buttonSender = new Button();
             textBoxSender = new TextBox();
             groupBoxRecieve = new GroupBox();
-            textBox1 = new TextBox();
+            textBoxReciever = new TextBox();
+            labelAuthor = new Label();
             groupBoxSetup.SuspendLayout();
             groupBoxSend.SuspendLayout();
             groupBoxRecieve.SuspendLayout();
@@ -51,10 +52,10 @@
             // 
             // labelDescription
             // 
-            labelDescription.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelDescription.Anchor = AnchorStyles.Top;
             labelDescription.AutoSize = true;
             labelDescription.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            labelDescription.Location = new Point(70, 9);
+            labelDescription.Location = new Point(72, 3);
             labelDescription.Name = "labelDescription";
             labelDescription.Size = new Size(200, 25);
             labelDescription.TabIndex = 0;
@@ -64,7 +65,7 @@
             // 
             labelMode.AutoSize = true;
             labelMode.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelMode.Location = new Point(26, 41);
+            labelMode.Location = new Point(32, 43);
             labelMode.Name = "labelMode";
             labelMode.Size = new Size(62, 21);
             labelMode.TabIndex = 1;
@@ -73,15 +74,15 @@
             // groupBoxSetup
             // 
             groupBoxSetup.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBoxSetup.Controls.Add(buttonDisconnect);
-            groupBoxSetup.Controls.Add(buttonConnect);
-            groupBoxSetup.Controls.Add(textBoxPort);
-            groupBoxSetup.Controls.Add(labelPort);
-            groupBoxSetup.Controls.Add(textBox2);
-            groupBoxSetup.Controls.Add(labelIP);
-            groupBoxSetup.Controls.Add(radioButtonModeServer);
-            groupBoxSetup.Controls.Add(radioButtonModeClient);
             groupBoxSetup.Controls.Add(labelMode);
+            groupBoxSetup.Controls.Add(radioButtonModeClient);
+            groupBoxSetup.Controls.Add(radioButtonModeServer);
+            groupBoxSetup.Controls.Add(labelIP);
+            groupBoxSetup.Controls.Add(textBoxIP);
+            groupBoxSetup.Controls.Add(labelPort);
+            groupBoxSetup.Controls.Add(textBoxPort);
+            groupBoxSetup.Controls.Add(buttonConnect);
+            groupBoxSetup.Controls.Add(buttonDisconnect);
             groupBoxSetup.Location = new Point(12, 40);
             groupBoxSetup.Name = "groupBoxSetup";
             groupBoxSetup.Size = new Size(320, 195);
@@ -89,123 +90,129 @@
             groupBoxSetup.TabStop = false;
             groupBoxSetup.Text = "Настройки";
             // 
-            // buttonDisconnect
-            // 
-            buttonDisconnect.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            buttonDisconnect.Location = new Point(12, 154);
-            buttonDisconnect.Name = "buttonDisconnect";
-            buttonDisconnect.Size = new Size(138, 23);
-            buttonDisconnect.TabIndex = 9;
-            buttonDisconnect.Text = "Отключиться";
-            buttonDisconnect.UseVisualStyleBackColor = true;
-            // 
-            // buttonConnect
-            // 
-            buttonConnect.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            buttonConnect.Location = new Point(18, 154);
-            buttonConnect.Name = "buttonConnect";
-            buttonConnect.Size = new Size(0, 23);
-            buttonConnect.TabIndex = 8;
-            buttonConnect.Text = "Соединиться";
-            buttonConnect.UseVisualStyleBackColor = true;
-            // 
-            // textBoxPort
-            // 
-            textBoxPort.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            textBoxPort.Location = new Point(155, 112);
-            textBoxPort.Name = "textBoxPort";
-            textBoxPort.Size = new Size(0, 23);
-            textBoxPort.TabIndex = 7;
-            // 
-            // labelPort
-            // 
-            labelPort.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            labelPort.AutoSize = true;
-            labelPort.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelPort.Location = new Point(155, 84);
-            labelPort.Name = "labelPort";
-            labelPort.Size = new Size(49, 21);
-            labelPort.TabIndex = 6;
-            labelPort.Text = "Порт:";
-            // 
-            // textBox2
-            // 
-            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            textBox2.Location = new Point(155, 49);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(0, 23);
-            textBox2.TabIndex = 5;
-            // 
-            // labelIP
-            // 
-            labelIP.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            labelIP.AutoSize = true;
-            labelIP.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            labelIP.Location = new Point(155, 21);
-            labelIP.Name = "labelIP";
-            labelIP.Size = new Size(73, 21);
-            labelIP.TabIndex = 4;
-            labelIP.Text = "IP-адрес:";
-            // 
-            // radioButtonModeServer
-            // 
-            radioButtonModeServer.AutoSize = true;
-            radioButtonModeServer.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            radioButtonModeServer.Location = new Point(46, 92);
-            radioButtonModeServer.Name = "radioButtonModeServer";
-            radioButtonModeServer.Size = new Size(80, 25);
-            radioButtonModeServer.TabIndex = 3;
-            radioButtonModeServer.Text = "Сервер";
-            radioButtonModeServer.UseVisualStyleBackColor = true;
-            // 
             // radioButtonModeClient
             // 
             radioButtonModeClient.AutoSize = true;
             radioButtonModeClient.Checked = true;
             radioButtonModeClient.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            radioButtonModeClient.Location = new Point(46, 65);
+            radioButtonModeClient.Location = new Point(52, 67);
             radioButtonModeClient.Name = "radioButtonModeClient";
             radioButtonModeClient.Size = new Size(78, 25);
             radioButtonModeClient.TabIndex = 2;
             radioButtonModeClient.TabStop = true;
             radioButtonModeClient.Text = "Клиент";
             radioButtonModeClient.UseVisualStyleBackColor = true;
+            radioButtonModeClient.CheckedChanged += radioButtonModeClient_CheckedChanged;
+            // 
+            // radioButtonModeServer
+            // 
+            radioButtonModeServer.AutoSize = true;
+            radioButtonModeServer.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButtonModeServer.Location = new Point(52, 94);
+            radioButtonModeServer.Name = "radioButtonModeServer";
+            radioButtonModeServer.Size = new Size(80, 25);
+            radioButtonModeServer.TabIndex = 3;
+            radioButtonModeServer.Text = "Сервер";
+            radioButtonModeServer.UseVisualStyleBackColor = true;
+            radioButtonModeServer.CheckedChanged += radioButtonModeServer_CheckedChanged;
+            // 
+            // labelIP
+            // 
+            labelIP.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelIP.AutoSize = true;
+            labelIP.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelIP.Location = new Point(161, 21);
+            labelIP.Name = "labelIP";
+            labelIP.Size = new Size(73, 21);
+            labelIP.TabIndex = 4;
+            labelIP.Text = "IP-адрес:";
+            // 
+            // textBoxIP
+            // 
+            textBoxIP.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxIP.Location = new Point(164, 49);
+            textBoxIP.MaxLength = 15;
+            textBoxIP.Name = "textBoxIP";
+            textBoxIP.Size = new Size(137, 23);
+            textBoxIP.TabIndex = 5;
+            // 
+            // labelPort
+            // 
+            labelPort.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelPort.AutoSize = true;
+            labelPort.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelPort.Location = new Point(161, 84);
+            labelPort.Name = "labelPort";
+            labelPort.Size = new Size(49, 21);
+            labelPort.TabIndex = 6;
+            labelPort.Text = "Порт:";
+            // 
+            // textBoxPort
+            // 
+            textBoxPort.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxPort.Location = new Point(164, 112);
+            textBoxPort.MaxLength = 5;
+            textBoxPort.Name = "textBoxPort";
+            textBoxPort.Size = new Size(137, 23);
+            textBoxPort.TabIndex = 7;
+            // 
+            // buttonConnect
+            // 
+            buttonConnect.Location = new Point(18, 153);
+            buttonConnect.Name = "buttonConnect";
+            buttonConnect.Size = new Size(138, 23);
+            buttonConnect.TabIndex = 8;
+            buttonConnect.Text = "Соединиться";
+            buttonConnect.UseVisualStyleBackColor = true;
+            buttonConnect.Click += buttonConnect_Click;
+            // 
+            // buttonDisconnect
+            // 
+            buttonDisconnect.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            buttonDisconnect.Location = new Point(164, 153);
+            buttonDisconnect.Name = "buttonDisconnect";
+            buttonDisconnect.Size = new Size(138, 23);
+            buttonDisconnect.TabIndex = 9;
+            buttonDisconnect.Text = "Отключиться";
+            buttonDisconnect.UseVisualStyleBackColor = true;
+            buttonDisconnect.Click += buttonDisconnect_Click;
             // 
             // groupBoxSend
             // 
-            groupBoxSend.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            groupBoxSend.Controls.Add(button1);
+            groupBoxSend.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBoxSend.Controls.Add(buttonSender);
             groupBoxSend.Controls.Add(textBoxSender);
             groupBoxSend.Location = new Point(12, 245);
             groupBoxSend.Name = "groupBoxSend";
-            groupBoxSend.Size = new Size(320, 75);
+            groupBoxSend.Size = new Size(320, 76);
             groupBoxSend.TabIndex = 3;
             groupBoxSend.TabStop = false;
             groupBoxSend.Text = "Отправка";
             // 
-            // button1
+            // buttonSender
             // 
-            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            button1.Location = new Point(87, 22);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 47);
-            button1.TabIndex = 1;
-            button1.Text = "Вперёд!";
-            button1.UseVisualStyleBackColor = true;
+            buttonSender.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonSender.Location = new Point(239, 21);
+            buttonSender.Name = "buttonSender";
+            buttonSender.Size = new Size(75, 48);
+            buttonSender.TabIndex = 1;
+            buttonSender.Text = "Вперёд!";
+            buttonSender.UseVisualStyleBackColor = true;
+            buttonSender.Click += buttonSender_Click;
             // 
             // textBoxSender
             // 
-            textBoxSender.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textBoxSender.Location = new Point(6, 22);
+            textBoxSender.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxSender.Location = new Point(6, 21);
             textBoxSender.Multiline = true;
             textBoxSender.Name = "textBoxSender";
-            textBoxSender.Size = new Size(75, 46);
+            textBoxSender.Size = new Size(227, 46);
             textBoxSender.TabIndex = 0;
             // 
             // groupBoxRecieve
             // 
-            groupBoxRecieve.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            groupBoxRecieve.Controls.Add(textBox1);
+            groupBoxRecieve.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBoxRecieve.Controls.Add(textBoxReciever);
             groupBoxRecieve.Location = new Point(12, 330);
             groupBoxRecieve.Name = "groupBoxRecieve";
             groupBoxRecieve.Size = new Size(320, 167);
@@ -213,28 +220,39 @@
             groupBoxRecieve.TabStop = false;
             groupBoxRecieve.Text = "Прослушка";
             // 
-            // textBox1
+            // textBoxReciever
             // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textBox1.Location = new Point(6, 22);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.ReadOnly = true;
-            textBox1.Size = new Size(156, 138);
-            textBox1.TabIndex = 0;
+            textBoxReciever.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxReciever.Location = new Point(6, 22);
+            textBoxReciever.Multiline = true;
+            textBoxReciever.Name = "textBoxReciever";
+            textBoxReciever.ReadOnly = true;
+            textBoxReciever.Size = new Size(308, 138);
+            textBoxReciever.TabIndex = 0;
+            // 
+            // labelAuthor
+            // 
+            labelAuthor.Anchor = AnchorStyles.Top;
+            labelAuthor.AutoSize = true;
+            labelAuthor.Location = new Point(139, 26);
+            labelAuthor.Name = "labelAuthor";
+            labelAuthor.Size = new Size(64, 15);
+            labelAuthor.TabIndex = 5;
+            labelAuthor.Text = "by dedalso";
             // 
             // DefaultForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(344, 509);
-            Controls.Add(groupBoxRecieve);
-            Controls.Add(groupBoxSend);
-            Controls.Add(groupBoxSetup);
             Controls.Add(labelDescription);
+            Controls.Add(labelAuthor);
+            Controls.Add(groupBoxSetup);
+            Controls.Add(groupBoxSend);
+            Controls.Add(groupBoxRecieve);
             Name = "DefaultForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "TCP/IP клиент";
+            Text = "TCP/IP прослушка";
             groupBoxSetup.ResumeLayout(false);
             groupBoxSetup.PerformLayout();
             groupBoxSend.ResumeLayout(false);
@@ -251,17 +269,18 @@
         private Label labelMode;
         private GroupBox groupBoxSetup;
         private GroupBox groupBoxSend;
-        private Button button1;
+        private Button buttonSender;
         private TextBox textBoxSender;
         private GroupBox groupBoxRecieve;
-        private TextBox textBox1;
+        private TextBox textBoxReciever;
         private RadioButton radioButtonModeServer;
         private RadioButton radioButtonModeClient;
         private Label labelIP;
-        private TextBox textBox2;
+        private TextBox textBoxIP;
         private TextBox textBoxPort;
         private Label labelPort;
         private Button buttonDisconnect;
         private Button buttonConnect;
+        private Label labelAuthor;
     }
 }
--- DefaultForm.cs
+++ DefaultForm.cs
@@ -1,19 +1,244 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace TCP_Client_Server
 {
     public partial class DefaultForm : Form
     {
         private int MinimalWidth = 360;
         private int MinimalHeight = 548;
+        private bool IsClient = true;
+        private bool IsConnected = false;
+        private IPAddress IP = IPAddress.Any;
+        private int Port;
+        private const int PortMaximum = 65536;
+        private TcpListener Listener;
+        private TcpClient Client;
 
         public DefaultForm()
         {
-            WindowSetup();
             InitializeComponent();
+            DefaultSetup();
+            FormSetup();
         }
 
-        private void WindowSetup()
+        private void FormSetup()
         {
             this.MinimumSize = new Size(MinimalWidth, MinimalHeight);
+            textBoxIP.Text = IP.ToString();
+            textBoxPort.Text = Port.ToString();
+            ConnectionLocker(false);
+        }
+
+        private void DefaultSetup()
+        {
+            //IP = SearchForIP();
+            Port = SearchForPort();
+        }
+
+        private bool SetIP()
+        {
+            string ip = textBoxIP.Text;
+            int status = ValidateIP(ip);
+
+            if (status == -1)
+            {
+                MessageBox.Show("Ошибка IP", "IP введён в некорректном формате.");
+                return false;
+            }
+            // Проверить IP
+            IP = IPAddress.Parse(ip);
+            return true;
+        }
+
+        private bool SetPort()
+        {
+            string port = textBoxPort.Text;
+            int status = ValidatePort(port);
+
+            if (status == -1)
+            {
+                MessageBox.Show("Ошибка порта", "Порт введён в нечисловом формате. Задайте порт от 0 до " + PortMaximum + ".");
+                return false;
+            }
+            else if (status == -2)
+            {
+                MessageBox.Show("Ошибка порта", "Невозможное значение порта. Задайте порт от 0 до " + PortMaximum + ".");
+                return false;
+            }
+            else if (status == -3)
+            {
+                MessageBox.Show("Ошибка порта", "Указанный порт занят. Выберите другой порт.");
+                return false;
+            }
+            Port = int.Parse(port);
+            return true;
+        }
+
+        private int ValidateIP(string ipWanted)
+        {
+            try
+            {
+                if (!IPAddress.TryParse(ipWanted, out _))
+                    return -1;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+            return 1;
+        }
+
+        private int ValidatePort(string portWanted)
+        {
+            int port;
+
+            try
+            {
+                port = int.Parse(portWanted);
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+            if (port > 0 && port < PortMaximum)
+            {
+                try
+                {
+                    TcpListener listener = new TcpListener(IP, port);
+                    listener.Start();
+                    listener.Stop();
+                }
+                catch (SocketException)
+                {
+                    return -3;
+                }
+            }
+            else
+                return -2;
+            return 1;
+        }
+
+        private IPAddress SearchForIP()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, Port);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            IPAddress ip = IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+            listener.Stop();
+            return ip;
+        }
+
+        private int SearchForPort()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+        private void ConnectionStart()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IP, Port);
+
+            if (IsClient)
+            {
+                Client = new TcpClient();
+                Client.ConnectAsync(endPoint);
+            }
+            else
+            {
+                Listener = new TcpListener(endPoint);
+                Listener.Start();
+            }
+        }
+
+        private void ConnectionCycle()
+        {
+            //while (true)
+            //{
+            Client = Listener.AcceptTcpClient();
+            NetworkStream stream = Client.GetStream();
+            byte[] getThis = new byte[100];
+            getThis = Encoding.Default.GetBytes("Get this!");
+            stream.Write(getThis, 0, getThis.Length);
+
+            //while (Client.Connected)
+            //{
+            byte[] message = new byte[1024];
+            int count = stream.Read(message, 0, message.Length);
+            textBoxReciever.Text = Encoding.Default.GetString(message, 0, count);
+            //}
+            //}
+        }
+
+        private void ConnectionStop()
+        {
+            if (!IsClient)
+                Listener.Stop();
+        }
+
+        private void ConnectionLocker(bool status)
+        {
+            if (status)
+            {
+                radioButtonModeClient.Enabled = false;
+                radioButtonModeServer.Enabled = false;
+                textBoxIP.Enabled = false;
+                textBoxPort.Enabled = false;
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = true;
+                buttonSender.Enabled = true;
+            }
+            else
+            {
+                radioButtonModeClient.Enabled = true;
+                radioButtonModeServer.Enabled = true;
+                textBoxIP.Enabled = true;
+                textBoxPort.Enabled = true;
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+                buttonSender.Enabled = false;
+            }
+        }
+
+        private void radioButtonModeClient_CheckedChanged(object sender, EventArgs e)
+        {
+            IsClient = true;
+        }
+
+        private void radioButtonModeServer_CheckedChanged(object sender, EventArgs e)
+        {
+            IsClient = false;
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!IsConnected)
+                if (SetIP() && SetPort())
+                {
+                    ConnectionStart();
+                    IsConnected = true;
+                    ConnectionLocker(true);
+                }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (IsConnected)
+            {
+                ConnectionStop();
+                IsConnected = false;
+                ConnectionLocker(false);
+            }
+        }
+
+        private void buttonSender_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
